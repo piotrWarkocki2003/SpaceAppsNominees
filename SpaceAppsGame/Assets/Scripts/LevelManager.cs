f38abe8c3e6f040e4001a179ff3b4e71793dae14
Dylan <123556990+BreezyEfficienC@users.noreply.github.com>
2023-10-14 17:31:28 +0100

    updated
    added layers for objects and tags for doors and change level manager script and fixed built settings

--- SpaceAppsGame/Assets/Scripts/LevelManager.cs
+++ SpaceAppsGame/Assets/Scripts/LevelManager.cs
@@ -7,22 +7,71 @@ public class LevelManager : MonoBehaviour
 {
     public GameObject doorT;
     public GameObject doorB;
-    private void OnTriggerEnter(Collider colision)
+    public GameObject doorO;
+
+    public GameObject doorT2;
+    public GameObject doorB2;
+    public GameObject doorO2;
+
+
+
+    private void Start()
+
     {
+
+        doorT = GameObject.FindWithTag("door");
+        doorB = GameObject.FindWithTag("door");
+        doorO = GameObject.FindWithTag("door");
+
+        doorT2 = GameObject.FindWithTag("door2");
+        doorB2 = GameObject.FindWithTag("door2");
+        doorO2 = GameObject.FindWithTag("door2");
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider colision)
+
+    {
+
         if (colision.tag == "level+")
+
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
+
+
         if (colision.tag == "level_")
+
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
 
-        if (colision.tag == "door")
+
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "door")
+
         {
             doorT.SetActive(false);
             doorB.SetActive(false);
+            doorO.SetActive(false);
+        }
+
+
+
+        if (collision.collider.tag == "door2")
+
+        {
+            doorT2.SetActive(false);
+            doorB2.SetActive(false);
+            doorO2.SetActive(false);
         }
     }
 }
