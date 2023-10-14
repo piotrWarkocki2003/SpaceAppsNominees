23d5ad906ad5d09d7170b12131c23df70a7fc5e6
Dylan <123556990+BreezyEfficienC@users.noreply.github.com>
2023-10-14 18:07:50 +0100

    update
    tags and layers updated 2.0

--- SpaceAppsGame/Assets/Scripts/LevelManager.cs
+++ SpaceAppsGame/Assets/Scripts/LevelManager.cs
@@ -12,44 +12,17 @@ public class LevelManager : MonoBehaviour
     public GameObject doorT2;
     public GameObject doorB2;
     public GameObject doorO2;
-
-
-
-    private void Start()
-
-    {
-
-        doorT = GameObject.FindWithTag("door");
-        doorB = GameObject.FindWithTag("door");
-        doorO = GameObject.FindWithTag("door");
-
-        doorT2 = GameObject.FindWithTag("door2");
-        doorB2 = GameObject.FindWithTag("door2");
-        doorO2 = GameObject.FindWithTag("door2");
-
-    }
-
-
-
     private void OnTriggerEnter(Collider colision)
-
     {
-
         if (colision.tag == "level+")
-
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
-
-
         if (colision.tag == "level_")
-
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
-
-
 
         
     }
@@ -57,17 +30,13 @@ public class LevelManager : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "door")
-
         {
             doorT.SetActive(false);
             doorB.SetActive(false);
             doorO.SetActive(false);
         }
 
-
-
         if (collision.collider.tag == "door2")
-
         {
             doorT2.SetActive(false);
             doorB2.SetActive(false);
